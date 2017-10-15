d0c25498426c03e293c31f85513946916fa766f2
Pedro Vieira <pacv@cin.ufpe.br>
2017-10-14 23:21:03 -0300

    1-7 done!

--- Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/domain/ItemFeed.java
+++ Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/domain/ItemFeed.java
@@ -3,19 +3,35 @@ package br.ufpe.cin.if710.podcast.domain;
 import java.io.Serializable;
 
 public class ItemFeed implements Serializable {
+    private final int id;
     private final String title;
     private final String link;
     private final String pubDate;
     private final String description;
     private final String downloadLink;
-
+    private String downloadUri;
+    private long downloadID;
 
     public ItemFeed(String title, String link, String pubDate, String description, String downloadLink) {
+        this.id = -1;
         this.title = title;
         this.link = link;
         this.pubDate = pubDate;
         this.description = description;
         this.downloadLink = downloadLink;
+    }
+
+    public ItemFeed(int id, String title, String link, String pubDate, String description, String downloadLink) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.pubDate = pubDate;
+        this.description = description;
+        this.downloadLink = downloadLink;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getTitle() {
@@ -36,6 +52,22 @@ public class ItemFeed implements Serializable {
 
     public String getDownloadLink() {
         return downloadLink;
+    }
+
+    public String getDownloadUri() {
+        return downloadUri;
+    }
+
+    public long getDownloadID() {
+        return downloadID;
+    }
+
+    public void setDownloadUri(String uri) {
+        this.downloadUri = uri;
+    }
+
+    public void setDownloadID(long id) {
+        this.downloadID = id;
     }
 
     @Override
--- Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/ui/adapter/XmlFeedAdapter.java
+++ Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/ui/adapter/XmlFeedAdapter.java
@@ -1,15 +1,20 @@
 package br.ufpe.cin.if710.podcast.ui.adapter;
 
-import java.util.List;
+import android.app.DownloadManager;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.List;
+
 import br.ufpe.cin.if710.podcast.R;
+import br.ufpe.cin.if710.podcast.db.io.PodcastProviderHelper;
 import br.ufpe.cin.if710.podcast.domain.ItemFeed;
 import br.ufpe.cin.if710.podcast.ui.EpisodeDetailActivity;
 
@@ -24,36 +29,11 @@ public class XmlFeedAdapter extends ArrayAdapter<ItemFeed> {
         this.context = context;
     }
 
-    /**
-     * public abstract View getView (int position, View convertView, ViewGroup parent)
-     * <p>
-     * Added in API level 1
-     * Get a View that displays the data at the specified position in the data set. You can either create a View manually or inflate it from an XML layout file. When the View is inflated, the parent View (GridView, ListView...) will apply default layout parameters unless you use inflate(int, android.view.ViewGroup, boolean) to specify a root view and to prevent attachment to the root.
-     * <p>
-     * Parameters
-     * position	The position of the item within the adapter's data set of the item whose view we want.
-     * convertView	The old view to reuse, if possible. Note: You should check that this view is non-null and of an appropriate type before using. If it is not possible to convert this view to display the correct data, this method can create a new view. Heterogeneous lists can specify their number of view types, so that this View is always of the right type (see getViewTypeCount() and getItemViewType(int)).
-     * parent	The parent that this view will eventually be attached to
-     * Returns
-     * A View corresponding to the data at the specified position.
-     */
-
-
-	/*
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View rowView = inflater.inflate(R.layout.itemlista, parent, false);
-		TextView textView = (TextView) rowView.findViewById(R.id.item_title);
-		textView.setText(items.get(position).getTitle());
-	    return rowView;
-	}
-	/**/
-
     //http://developer.android.com/training/improving-layouts/smooth-scrolling.html#ViewHolder
     static class ViewHolder {
         TextView item_title;
         TextView item_date;
+        Button item_action;
     }
 
     @Override
@@ -61,9 +41,12 @@ public class XmlFeedAdapter extends ArrayAdapter<ItemFeed> {
         ViewHolder holder;
         if (convertView == null) {
             convertView = View.inflate(getContext(), linkResource, null);
+
             holder = new ViewHolder();
             holder.item_title = (TextView) convertView.findViewById(R.id.item_title);
             holder.item_date = (TextView) convertView.findViewById(R.id.item_date);
+            holder.item_action = (Button) convertView.findViewById(R.id.item_action);
+
             convertView.setTag(holder);
             convertView.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -71,6 +54,20 @@ public class XmlFeedAdapter extends ArrayAdapter<ItemFeed> {
                     Intent intent = new Intent(context, EpisodeDetailActivity.class);
                     intent.putExtra(EpisodeDetailActivity.ITEM_FEED, getItem(position));
                     context.startActivity(intent);
+                }
+            });
+
+            holder.item_action.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getContext(),"Baixando podcast", Toast.LENGTH_SHORT).show();
+                    // solicita o download manager do sistema para fazer o download do podcast
+                    DownloadManager downloadManager = (DownloadManager) getContext().getSystemService(Context.DOWNLOAD_SERVICE);
+                    ItemFeed itemFeed = getItem(position);
+                    // coloca o download numa fila e recupera seu ID para recuperar o arquivo posteriormente
+                    long downloadID = downloadManager.enqueue(new DownloadManager.Request(Uri.parse(itemFeed.getDownloadLink())));
+                    // salva o ID no BD
+                    PodcastProviderHelper.updateDownloadID(getContext(), itemFeed.getId(), downloadID);
                 }
             });
         } else {
